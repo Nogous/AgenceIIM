e02a69d21836ed2089aa3e2f4066181f0c2b2b14
Camille <camille.lavarde@gmail.com>
2020-04-07 16:06:27 +0200

    reset

---
+++ AgenceIIM/Assets/Resources/Scripts/Cube.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    private Vector3 initPos;
+    private Quaternion initRot;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        initPos = transform.position;
+        initPos.x = (int)initPos.x;
+        initPos.y = (int)initPos.y;
+        initPos.z = (int)initPos.z;
+
+        transform.position = initPos;
+        initRot = transform.rotation;
+    }
+
+    private void Start()
+    {
+        GameManager.instance.AddCube(this);
+    }
+
+    public void ResetCube()
+    {
+        transform.position = initPos;
+        transform.rotation = initRot;
+    }
+}
--- AgenceIIM/Assets/Resources/Scripts/GameManager.cs
+++ AgenceIIM/Assets/Resources/Scripts/GameManager.cs
@@ -9,6 +9,7 @@ public class GameManager : MonoBehaviour
     [SerializeField] private KeyCode resetGameKey = KeyCode.R;
 
     public Player player;
+    private List<Cube> cubes = new List<Cube>();
 
     private void Awake()
     {
@@ -27,6 +28,23 @@ public class GameManager : MonoBehaviour
         if (Input.GetKeyDown(resetGameKey))
         {
             player.Reset();
+            ResetCubes();
         }
     }
+
+    #region Cube
+    public void AddCube(Cube cube)
+    {
+        cubes.Add(cube);
+    }
+
+    private void ResetCubes()
+    {
+        for (int i = cubes.Count; i-->0;)
+        {
+            cubes[i].gameObject.SetActive(true);
+            cubes[i].ResetCube();
+        }
+    }
+    #endregion
 }
