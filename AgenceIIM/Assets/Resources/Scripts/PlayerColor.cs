d01129b0e8b134a773beefcdb75adfa560e46655
Camille <camille.lavarde@gmail.com>
2020-04-07 09:19:44 +0200

    take color

--- AgenceIIM/Assets/Resources/Scripts/PlayerColor.cs
+++ AgenceIIM/Assets/Resources/Scripts/PlayerColor.cs
@@ -12,90 +12,26 @@ public enum MoveDir
 
 public class PlayerColor : MonoBehaviour
 {
-    // controles
-    public KeyCode up = KeyCode.W;
-    public KeyCode down = KeyCode.S;
-    public KeyCode right = KeyCode.D;
-    public KeyCode left = KeyCode.A;
+    public Player player;
+    public Transform focusBottom;
 
-    // cube
-    public Renderer[] faceColor;
-    // top, bottom, left, up, down, right
-    //    3
-    //  2 0 5 1 
-    //    4
-
-    private void Start()
+    public void Update()
     {
-        faceColor[0].material.color = Color.red;
+        TestGround();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void TestGround()
     {
-        if (Input.GetKeyDown(up))
-        {
-            UpdateColor(MoveDir.up);
-            //    0
-            //  2 4 5 3
-            //    1
-        }
-        if (Input.GetKeyDown(down))
-        {
-            UpdateColor(MoveDir.down);
-            //    1
-            //  2 3 5 4
-            //    0
-        }
-        if (Input.GetKeyDown(right))
-        {
-            UpdateColor(MoveDir.right);
-            //    3
-            //  1 2 0 5 
-            //    4
-        }
-        if (Input.GetKeyDown(left))
-        {
-            UpdateColor(MoveDir.left);
-            //    3
-            //  0 5 1 2 
-            //    4
-        }
-    }
-    //    3
-    //  2 0 5 1 
-    //    4
+        Ray ray = new Ray(player.transform.position, focusBottom.localPosition);
+        Debug.DrawRay(player.transform.position, focusBottom.localPosition);
+        RaycastHit hit;
 
-    public void UpdateColor(MoveDir moveDir)
-    {
-        Color tmpColor = faceColor[0].material.color;
-
-        switch (moveDir)
+        if (Physics.Raycast(ray, out hit ,1f))
         {
-            case MoveDir.up:
-                faceColor[0].material.color = faceColor[4].material.color;
-                faceColor[4].material.color = faceColor[1].material.color;
-                faceColor[1].material.color = faceColor[3].material.color;
-                faceColor[3].material.color = tmpColor;
-                break;
-            case MoveDir.down:
-                faceColor[0].material.color = faceColor[3].material.color;
-                faceColor[3].material.color = faceColor[1].material.color;
-                faceColor[1].material.color = faceColor[4].material.color;
-                faceColor[4].material.color = tmpColor;
-                break;
-            case MoveDir.right:
-                faceColor[0].material.color = faceColor[2].material.color;
-                faceColor[2].material.color = faceColor[1].material.color;
-                faceColor[1].material.color = faceColor[5].material.color;
-                faceColor[5].material.color = tmpColor;
-                break;
-            case MoveDir.left:
-                faceColor[0].material.color = faceColor[5].material.color;
-                faceColor[5].material.color = faceColor[1].material.color;
-                faceColor[1].material.color = faceColor[2].material.color;
-                faceColor[2].material.color = tmpColor;
-                break;
+            if (hit.transform.gameObject.CompareTag("Color"))
+            {
+                player.faceColor[1].GetComponent<Renderer>().material.color = hit.transform.gameObject.GetComponent<Renderer>().material.color;
+            }
         }
     }
 }
