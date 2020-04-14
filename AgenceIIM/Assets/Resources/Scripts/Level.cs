5d9667ffdfdb93ff22d6fc38525ebda69382a86b
Camille <camille.lavarde@gmail.com>
2020-04-14 11:06:04 +0200

    fix simulate axis rewired

---
+++ AgenceIIM/Assets/Resources/Scripts/Level.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(10, 10, 150, 100), "I am a button"))
+        {
+            print("You clicked the button!");
+        }
+    }
+}
