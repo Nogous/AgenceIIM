b9c4e6efe2bfda60165ce43abe9766df7b9791df
Camille <camille.lavarde@gmail.com>
2020-04-16 14:16:41 +0200

    yolo lerp

---
+++ AgenceIIM/Assets/Resources/Scripts/YoloLerp.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YoloLerp : MonoBehaviour
+{
+    public GameObject obj;
+    public float speed = .1f;
+
+    public GameObject p1;
+    public GameObject p2;
+    public GameObject angle;
+
+    float lerpAdvencement = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lerpAdvencement += Time.deltaTime * speed;
+
+        Vector3 p1Cur = Vector3.Lerp(p1.transform.position, angle.transform.position, lerpAdvencement);
+        Vector3 p2Cur = Vector3.Lerp(angle.transform.position, p2.transform.position, lerpAdvencement);
+
+        obj.transform.position = Vector3.Lerp(p1Cur, p2Cur, lerpAdvencement);
+
+        if (lerpAdvencement > 1)
+        {
+            lerpAdvencement = 0;
+        }
+    }
+}
