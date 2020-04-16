c1f11f20050ac3630ac6a2956e92c7b0d708bc4d
DrebinN78 <dorian.gelas@gmail.com>
2020-04-16 15:20:24 +0200

    Camera rotative

--- AgenceIIM/Assets/Resources/Scripts/CameraHandler.cs
+++ AgenceIIM/Assets/Resources/Scripts/CameraHandler.cs
@@ -5,13 +5,15 @@ public class CameraHandler : MonoBehaviour
 {
     public static CameraHandler instance = null;
     bool travel;
+    [System.NonSerialized]
     public bool position;
     float progress = 0.0f;
     public GameObject cameraGO;
     public Vector3 positionDepart;
     public Vector3 positionAlternatif;
-    public Vector3 TanDepart;
-    public Vector3 TanAlternatif;
+    public Vector3 positionTan;
+    Vector3 vectorTan;
+    Vector3 vectorNormal;
     [Range(2, 100)] 
     public int slowFactor;
     [Range(0.01f, 0.90f)] 
@@ -41,6 +43,7 @@ public class CameraHandler : MonoBehaviour
         }
         positionDepart = cameraGO.transform.position;
         positionAlternatif = GameObject.Find("Point_Alt").transform.position;
+        positionTan = GameObject.Find("Point_Tan").transform.position;
         if (GameObject.Find("Point_Alt") == null)
         {
             Debug.LogError("Sonde de position alternative manquante, traveling indisponible");
@@ -61,42 +64,35 @@ public class CameraHandler : MonoBehaviour
     }
     public void Travel()
     {
-        if (progress < cutoff)
+        
+
+        if (progress < 1.0f)
         {
             switch (position)
             {
-                case true:
-                {
-                    progress += (Time.deltaTime / slowFactor);
-                    cameraGO.transform.position = Vector3.Lerp(cameraGO.transform.position, positionDepart, progress);
-                    
-                }
-                break;
                 case false:
                 {
-                    progress += (Time.deltaTime / slowFactor);
-                    cameraGO.transform.position = Vector3.Lerp(cameraGO.transform.position, positionAlternatif, progress);
-                    
+                    progress += (Time.deltaTime * slowFactor);
+                        vectorNormal = Vector3.Lerp(positionDepart, positionTan, progress);
+                        vectorTan = Vector3.Lerp(positionTan, positionAlternatif, progress);
+                        cameraGO.transform.position = Vector3.Lerp(vectorNormal, vectorTan, progress); 
+                }
+                break;
+                case true:
+                {
+                    progress += (Time.deltaTime * slowFactor);
+                        vectorNormal = Vector3.Lerp(positionDepart, positionTan, progress);
+                        vectorTan = Vector3.Lerp(positionTan, positionAlternatif, progress);
+                        cameraGO.transform.position = Vector3.Lerp(vectorNormal, vectorTan, -progress);
                 }
                 break;
             }
         }
-        else
+        else if (progress >= 1.0f)
         {
-            if (!position)
-            {
-                
-                cameraGO.transform.position = positionAlternatif;
-                position = true;
-            }
-            else
-            {
-                
-                cameraGO.transform.position = positionDepart;
-                position = false;
-            }
+            progress = 0.0f;
             travel = false;
-            progress = 0f;            
+            position = !position;
         }
     }
 }
