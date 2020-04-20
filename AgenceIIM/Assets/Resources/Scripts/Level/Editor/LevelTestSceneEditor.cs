f6a15ca1cefd101dc5e166d395075a05d68e689d
Camille <camille.lavarde@gmail.com>
2020-04-20 17:14:32 +0200

    save in inspector

---
+++ AgenceIIM/Assets/Resources/Scripts/Level/Editor/LevelTestSceneEditor.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+
+[CustomEditor(typeof(LevelTestScene))]
+public class LevelTestSceneEditor : Editor
+{
+    LevelTestScene level;
+
+    private void OnEnable()
+    {
+        level = (LevelTestScene)target;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        if (GUILayout.Button("Load"))
+        {
+            level.OnClickLoadLevel();
+        }
+        if (GUILayout.Button("Save"))
+        {
+            level.OnClickSaveLevel();
+        }
+    }
+}
