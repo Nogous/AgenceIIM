9809a7a8380d8abc621dcde47d5547c330c520fc
François <dessartsfran@gmail.com>
2020-04-21 17:23:40 +0200

    Camera effects

---
+++ AgenceIIM/Assets/Resources/Scripts/SplashCamera.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+
+public class SplashCamera : MonoBehaviour
+{
+    [SerializeField] private List<Sprite> stains = new List<Sprite>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CreateStains(Color paintColor)
+    {
+        int stainNum;
+
+        stainNum = (int)Random.Range(3f, 8f);
+
+        for(int i = 0; i < stainNum; i++)
+        {
+            GameObject stain = new GameObject();
+            stain.AddComponent<SpriteRenderer>();
+            SpriteRenderer renderer = stain.GetComponent<SpriteRenderer>();
+
+            renderer.sprite = stains[(int)Random.value];
+            renderer.color = paintColor;
+        }
+    }
+}
