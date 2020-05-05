d200560b04e90aa97183cb84ff6ce41a85bcd212
Camille <camille.lavarde@gmail.com>
2020-05-05 15:20:51 +0200

    up

---
+++ AgenceIIM/Assets/Resources/Scripts/Menu/Menu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+    public int nbLevel = 11;
+    private int currentId = 0;
+
+    // affichage info level
+    public Text nameLeveltext = null;
+    public Text nbcouptext = null;
+    public Text nbEnnemietext = null;
+
+    public void OnClickStartLevel()
+    {
+        SceneManager.LoadScene("Level " + currentId + ".1");
+    }
+
+    public void OnclikSelecteLevel(int i)
+    {
+        currentId += i;
+        if (currentId >= nbLevel)
+        {
+            currentId -= nbLevel;
+        }
+        if (currentId < 0)
+        {
+            currentId = nbLevel + i;
+        }
+
+        nameLeveltext.text = "Level 1-" + currentId;
+    }
+}
