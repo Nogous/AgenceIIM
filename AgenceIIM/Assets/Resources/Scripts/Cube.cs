1ffba85128ab85f7a0aeecbd89b6061cf34615b3
Carlos <leonardjeannerot@icloud.com>
2020-05-05 15:18:10 +0200

    Change script

--- AgenceIIM/Assets/Resources/Scripts/Cube.cs
+++ AgenceIIM/Assets/Resources/Scripts/Cube.cs
@@ -548,7 +548,7 @@ public class Cube : MonoBehaviour
             if (hit.transform.gameObject.GetComponent<Cube>())
             {
                 Cube tmpCube = hit.transform.gameObject.GetComponent<Cube>();
-
+ 
                 tmpCube.Explode();
             }
             else if (hit.transform.parent != null)
@@ -593,7 +593,7 @@ public class Cube : MonoBehaviour
         Destroy(piece, 1.5f);
     }
 
-    #endregion
+    #endregion 
 
     private void Update()
     {
