4289a4ef84f1585b12cc2ec065636ca060b48eb3
Camille <camille.lavarde@gmail.com>
2020-05-20 16:09:58 +0200

    menu ++

---
+++ AgenceIIM/Assets/LevelUI.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelUI : MonoBehaviour
+{
+    public int idLevel;
+    public Text idLevelText;
+
+    public GameObject starUnlock1;
+    public GameObject starUnlock2;
+    public GameObject starUnlock3;
+
+    public GameObject locker;
+}
