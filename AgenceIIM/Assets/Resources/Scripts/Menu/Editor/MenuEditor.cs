f4ac6ced868953406b2c76020409ff44c75079e5
Camille <camille.lavarde@gmail.com>
2020-05-25 14:26:47 +0200

    unlock easy

--- AgenceIIM/Assets/Resources/Scripts/Menu/Editor/MenuEditor.cs
+++ AgenceIIM/Assets/Resources/Scripts/Menu/Editor/MenuEditor.cs
@@ -7,9 +7,11 @@ using UnityEditor;
 [CanEditMultipleObjects]
 public class MenuEditor : Editor
 {
+    Menu myMenu;
 
     void OnEnable()
     {
+        myMenu = (Menu)target;
     }
 
     public override void OnInspectorGUI()
@@ -21,6 +23,31 @@ public class MenuEditor : Editor
             SaveSystem.SavePoints(new int[0], "starsMonde1");
             SaveSystem.SavePoints(new int[0], "starsMonde2");
             SaveSystem.SavePoints(new int[0], "starsMonde3");
+        }
+
+        if (GUILayout.Button("Unlock All"))
+        {
+            for (int i = 0; i < myMenu.levelUIMonde1.Length; i++)
+            {
+                if (myMenu.levelUIMonde1[i].locker != null)
+                {
+                    myMenu.levelUIMonde1[i].locker.SetActive(false);
+                }
+            }
+            for (int i = 0; i < myMenu.levelUIMonde2.Length; i++)
+            {
+                if (myMenu.levelUIMonde2[i].locker != null)
+                {
+                    myMenu.levelUIMonde2[i].locker.SetActive(false);
+                }
+            }
+            for (int i = 0; i < myMenu.levelUIMonde3.Length; i++)
+            {
+                if (myMenu.levelUIMonde3[i].locker != null)
+                {
+                    myMenu.levelUIMonde3[i].locker.SetActive(false);
+                }
+            }
         }
     }
 }
