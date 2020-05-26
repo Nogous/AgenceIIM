dbd1344bd15c8748176c28dc5bc07fa5afac3d02
Camille <camille.lavarde@gmail.com>
2020-05-26 10:21:04 +0200

    end level menu

---
+++ AgenceIIM/Assets/Resources/Scripts/Level/UIEndLevel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIEndLevel : MonoBehaviour
+{
+    public void OnClickLoadNextLevel()
+    {
+        GameManager.instance.LoadNextScene();
+    }
+
+    public void OnClickRestartLevel()
+    {
+        GameManager.instance.ResetParty();
+    }
+
+    public void OnClickBackMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
