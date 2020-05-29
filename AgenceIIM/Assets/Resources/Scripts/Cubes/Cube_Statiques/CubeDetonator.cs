123d24ccb4b072b77fdf432518d4c970faea3978
Dorian Gélas <dorian.gelas@gmail.com>
2020-05-29 11:47:58 +0200

    Auto stash before merge of "master" and "origin/master"

--- AgenceIIM/Assets/Resources/Scripts/Cubes/Cube_Statiques/CubeDetonator.cs
+++ AgenceIIM/Assets/Resources/Scripts/Cubes/Cube_Statiques/CubeDetonator.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class CubeDetonator : CubeStatic
 {
+    public enum fuseOrientationMode{
+        HorrizontalFirst,
+        VerticalFirst,
+    }
+    public float fuseOffset;
+    public bool fuseOrientation;
+    public fuseOrientationMode fuseManhanttanOrder;
+    public Color fuseColor;
     public CubeTNT associatedTnt;
 
     public override void OnAwake()
@@ -22,4 +30,20 @@ public class CubeDetonator : CubeStatic
             associatedTnt.Explode();
         }
     }
+
+    public void DrawFuse(Color fuseColor)
+    {
+        if(fuseManhanttanOrder == fuseOrientationMode.HorrizontalFirst){
+            Debug.DrawRay(new Vector3(transform.position.x, transform.position.y + fuseOffset, transform.position.z), transform.TransformVector(Vector3.forward) * (System.Math.Abs(associatedTnt.transform.position.x - transform.position.x)), fuseColor);
+            Debug.DrawRay(new Vector3(System.Math.Abs(associatedTnt.transform.position.x - transform.position.x), transform.position.y + fuseOffset, transform.position.z), associatedTnt.transform.position, fuseColor);
+        }
+        
+    }
+
+    [ExecuteInEditMode]
+    void Update()
+    {
+        DrawFuse(fuseColor);
+    }
+
 }
