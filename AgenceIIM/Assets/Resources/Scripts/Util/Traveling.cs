9ac11d3029674ecd741f7137686c91a31c502f39
Camille <camille.lavarde@gmail.com>
2020-06-26 14:37:25 +0200

    traveling

---
+++ AgenceIIM/Assets/Resources/Scripts/Util/Traveling.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Traveling : MonoBehaviour
+{
+    public Transform ObjToMove;
+
+    public Transform initPos;
+    public Transform endPos;
+    public float speed;
+
+    public bool playOnStart = false;
+    public KeyCode keyStart = KeyCode.Space;
+
+    public bool canDoAgain;
+    private bool isTraveling = false;
+    private float lerpCount = 0f;
+
+    private void Start()
+    {
+        StartTravel();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(keyStart))
+        {
+            StartTravel();
+        }
+
+        if (isTraveling)
+        {
+            DoTraveling();
+        }
+    }
+
+    public void StartTravel()
+    {
+        if (isTraveling && !canDoAgain) return;
+
+        isTraveling = true;
+        lerpCount = 0f;
+    }
+
+    private void DoTraveling()
+    {
+        lerpCount += Time.deltaTime * speed;
+        ObjToMove.position = Vector3.Lerp(initPos.position, endPos.position, lerpCount);
+    }
+}
